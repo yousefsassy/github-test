33f5227d5825c7b13724c53174e7f61dc1621a61
yousefsassy <youssef.sassi@medtech.tn>
2022-10-06 14:35:32 +0100

    Initial Commit

---
+++ github test/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  github test
+//
+//  Created by Youssef Sassi on 6/10/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
